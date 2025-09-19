5dc0fda1889a82913fd357042633791bb0a373bc
Alejandro <acantelo7@alumnes.ub.edu>
2025-09-19 14:35:52 +0200

    fix: Lsp fixed, working now with Mason.

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,7 +32,7 @@ vim.opt.showbreak = "↪ "
 vim.opt.list = true
 
 --vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- Tab icon removed in next line
-vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "  ", trail = " ", nbsp = "␣" }
  
 
 vim.api.nvim_create_autocmd("VimEnter", {
@@ -204,19 +204,97 @@ end,
       })
     end,
   },
+    {
+        {
+            'williamboman/mason.nvim',
+            lazy = false,
+            opts = {},
+        },
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.pyright.setup { capabilities = capabilities } -- Python
-      lspconfig.ts_ls.setup { capabilities = capabilities } -- TypeScript/JavaScript
-      lspconfig.clangd.setup { capabilities = capabilities }-- C/C++
-      lspconfig.html.setup { capabilities = capabilities } -- html
-      lspconfig.cssls.setup { capabilities = capabilities } -- CSS
-    end,
-  },
+        -- Autocompletion
+        {
+            'hrsh7th/nvim-cmp',
+            event = 'InsertEnter',
+            config = function()
+                local cmp = require('cmp')
+
+                cmp.setup({
+                    sources = {
+                        {name = 'nvim_lsp'},
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    }),
+                    snippet = {
+                        expand = function(args)
+                            vim.snippet.expand(args.body)
+                        end,
+                    },
+                })
+            end
+        },
+
+        -- LSP
+        {
+            'neovim/nvim-lspconfig',
+            cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+            event = {'BufReadPre', 'BufNewFile'},
+            dependencies = {
+                {'hrsh7th/cmp-nvim-lsp'},
+                {'williamboman/mason.nvim'},
+                {'williamboman/mason-lspconfig.nvim'},
+            },
+            init = function()
+                -- Reserve a space in the gutter
+                -- This will avoid an annoying layout shift in the screen
+                vim.opt.signcolumn = 'yes'
+            end,
+            config = function()
+                local lsp_defaults = require('lspconfig').util.default_config
+
+                -- Add cmp_nvim_lsp capabilities settings to lspconfig
+                -- This should be executed before you configure any language server
+                lsp_defaults.capabilities = vim.tbl_deep_extend(
+                    'force',
+                    lsp_defaults.capabilities,
+                    require('cmp_nvim_lsp').default_capabilities()
+                )
+
+                -- LspAttach is where you enable features that only work
+                -- if there is a language server active in the file
+                vim.api.nvim_create_autocmd('LspAttach', {
+                    desc = 'LSP actions',
+                    callback = function(event)
+                        local opts = {buffer = event.buf}
+
+                        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                    end,
+                })
+
+                require('mason-lspconfig').setup({
+                    ensure_installed = {},
+                    handlers = {
+                        -- this first function is the "default handler"
+                        -- it applies to every language server without a "custom handler"
+                        function(server_name)
+                            require('lspconfig')[server_name].setup({})
+                        end,
+                    }
+                })
+            end,
+        }
+    },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
